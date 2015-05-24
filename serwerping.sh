1f347e4df50ceaeb3a58aa9bc7562c1044b9bd26
tomaaszq <tomaaszq95@gmail.com>
2015-05-24 12:41:11 +0200

    changed some polish words on english language

--- serwerping.sh
+++ serwerping.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-touch statusy.csv
-echo "Nazwa serwera,status" >> statusy.csv
-for Serwer1 in $(cat lista.txt)
+touch status.csv
+echo "Nazwa serwera,status" >> status.csv
+for Serwer1 in $(cat list.txt)
 do
 	ping -c 1 $Serwer1
 	if (( $? == 0));then
-	echo "$Serwer1,odpowiada" >> statusy.csv
+	echo "$Serwer1,works fine" >> statusy.csv
 else
-	echo "$Serwer1,nie odpowiada" >> statusy.csv
+	echo "$Serwer1,doesn't work :(" >> statusy.csv
 fi
 done
